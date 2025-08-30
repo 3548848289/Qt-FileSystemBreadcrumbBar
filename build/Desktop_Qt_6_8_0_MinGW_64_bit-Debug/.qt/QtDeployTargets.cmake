824937f0c30123da8f7d203302d31a551cca3113
QiHan <3548848289@qq.com>
2025-08-30 21:53:59 +0800

    清理无用文件

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BreadcrumbBar_FILE D:/QT6/Qt_pro/202508Train/BreadcrumbBar/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/BreadcrumbBar.exe)
-set(__QT_DEPLOY_TARGET_BreadcrumbBar_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BreadcrumbBar_RUNTIME_DLLS D:/QT6/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/QT6/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/QT6/6.8.0/mingw_64/bin/Qt6Core.dll)
